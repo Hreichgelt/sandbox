6b1215b1082418db022c51010d7e5a3c423781e7
Hans Reichgelt <hreichgelt06@gmail.com>
2023-03-27 17:33:56 -0500

    freelance set budget

--- freelance.js
+++ freelance.js
@@ -6,13 +6,12 @@ console.log(dayRate(75, 8));
 
 
 // Another day, a project manager offers the freelancer to work on a project with a fixed budget. Given the fixed budget and the freelancer's hourly rate, help them calculate the number of days they would work until the budget is exhausted. The result must be rounded down to the nearest whole number.
-function daysInBudget(budget, hourlyrate) {
-    const totalbudget = [20000];
-    const daycost = Math.floor(budget - hourlyrate)
-    for (i = totalbudget; i < totalbudget.length; i--){
-        return daycost
-    }
-
+    // const totalbudget = [20000];
+    // hourly cost = $75
+    // daily cost = $75 * 8h = $600
+    // 20000 / 600 = 33.33 days 
+function daysInBudget(budget, dayRate) {
+    return Math.floor(budget / dayRate)
 };
 console.log(daysInBudget(20000, 75));
 
