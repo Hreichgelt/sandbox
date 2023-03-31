823dccc3f9da8adec0ee21454c57c2ca1f6caaa6
Hans Reichgelt <hreichgelt06@gmail.com>
2023-03-30 19:31:55 -0500

    birds

--- birds.js
+++ birds.js
@@ -1,11 +1,19 @@
 // Let us start analyzing the data by getting a high-level view. Find out how many birds you counted in total since you started your logs.
 // Implement a function totalBirdCount that accepts an array that contains the bird count per day. It should return the total number of birds that you counted.
-birdsPerDay = [2, 5, 0, 7, 4, 1, 3, 0, 2, 5, 0, 1, 3, 1];
-function totalBirdCount(birdsPerDay) {
-    let total = [];
-    for (i = 0; i < birdsPerDay.length; i++) {
-        return total + birdsPerDay[i];
-    }
-}
+let birdsPerDay = [2, 5, 0, 7, 4, 1, 3, 0, 2, 5, 0, 1, 3, 1];
 
-console.log(totalBirdCount(birdsPerDay));
+let totalBirdCount = birdsPerDay.reduce((birdsPerDay, a) => {
+return birdsPerDay + a;
+})
+
+console.log(totalBirdCount);
+
+
+// Now that you got a general feel for your bird count numbers, you want to make a more fine-grained analysis.
+
+// Implement a function birdsInWeek that accepts an array of bird counts per day and a week number. It returns the total number of birds that you counted in that specific week. You can assume weeks are always tracked completely.
+
+// let birdsPerDay = [2, 5, 0, 7, 4, 1, 3, 0, 2, 5, 0, 1, 3, 1];
+
+
+birdsInWeek(birdsPerDay, 2);
