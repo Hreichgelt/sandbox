d1a26505b19650e303a09fe0b5800d06b4f3a1d7
Hans Reichgelt <hreichgelt06@gmail.com>
2023-04-04 03:36:21 -0500

    carchoice

--- license.js
+++ license.js
@@ -9,4 +9,18 @@ function needsLicense(vehicle) {
         return false
     }
 };
-console.log(needsLicense('car'))
+console.log(needsLicense('car'));
+
+// You evaluate your options of available vehicles. You manage to narrow it down to two options but you need help making the final decision. For that implement the function chooseVehicle(option1, option2) that takes two vehicles as arguments and returns a decision that includes the option that comes first in alphabetical order.
+
+// chooseVehicle('Wuling Hongguang', 'Toyota Corolla');
+// chooseVehicle('Volkswagen Beetle', 'Volkswagen Golf');
+
+function chooseVehicle(option1, option2) {
+    if (option1 > option2) {
+        return option1
+    } else {
+        return option2
+    };
+};
+console.log(chooseVehicle('Corolla', 'Infiniti'))
